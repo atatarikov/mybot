d0b499fc00b9faf8baef5ec8e03021bc23a23643
Andrey Tatarikov <atatarikov@gmail.com>
2022-05-18 22:17:58 +0300

    fir2

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@
 import logging
 from telegram.ext import Updater,CommandHandler,MessageHandler, Filters
 import settings
+import ephem
+from datetime import date
 
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
@@ -17,6 +19,25 @@ def greet_user(update, context):
     # print(update.from.)
     update.message.reply_text("Привет, пользователь! Ты вызвал команду /start")
 
+def where_planet(name_planet):
+    current_date = str(date.today()).replace('-','/')
+  
+    try:
+        if name_planet.upper() == 'MARS':
+            return ephem.constellation(ephem.Mars(current_date))
+        elif name_planet.lower() == 'earth':
+            return ephem.constellation(ephem.Earth(current_date))
+        else:
+            return 'Не обрабатываем такую планету'
+    except:
+        return "Не могу определить планету"
+    
+
+
+def planet_user(update, context):
+    name_planet = update.message.text.split()[1]
+    update.message.reply_text(where_planet(name_planet))
+
 def talk_to_me(update, context):
     user_text = update.message.text 
     print(user_text)
@@ -29,6 +50,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     logging.info('Бот стартовал')
     # Командуем боту начать ходить в Telegram за сообщениями
